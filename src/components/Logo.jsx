87b3b85b47c0dce5929cf5767ea2d8f768ec9f88
Mihail Golban <mihail.golban@communityfibre.co.uk>
2022-11-07 23:20:12 +0200

    feat: add svg images

---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+import logoSvg from "../assets/logo.svg";
+
+export const Logo = () => {
+  return <img src={logoSvg} height="35px" className="logo react" alt="React logo" />;
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#224DE5",
+      secondary: "#F15A24",
+    },
   },
   plugins: [],
 };
