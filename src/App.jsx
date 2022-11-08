268963c0fe4554af44bdc72a308b772052f87ef9
Mihail Golban <mihail.golban@communityfibre.co.uk>
2022-11-09 01:42:53 +0200

    feat: added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header/Header";
+import { HeroSection } from "./components/sections/hero-section";
 
 function App() {
   return (
     <>
       <Header />
+      <HeroSection />
     </>
   );
 }
