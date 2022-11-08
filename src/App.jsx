90bfa38aa646993a676dc323f26ee5ed1384ce8e
Mihail Golban <mihail.golban@communityfibre.co.uk>
2022-11-08 23:30:22 +0200

    feat add button and nav links

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import "./App.css";
-import { ReactComponent as Programmer } from "./assets/programmer.svg";
+import { Button } from "./components/UI/Button/Button";
+import { NavLink } from "./components/UI/NavLink/NavLink";
+import { MdMailOutline, MdOutlineFileDownload } from "react-icons/md";
 
 function App() {
   return (
-    <div>
-      <Programmer />
-    </div>
+    <>
+      <div className="p-3">
+        <Button primary>Hire me</Button>
+        <Button primary icon={MdMailOutline} iconPosition="right">
+          Hire me
+        </Button>
+        <Button secondary icon={MdMailOutline}>
+          Contact me
+        </Button>
+        <Button primary size="lg" icon={MdMailOutline}>
+          Contact me
+        </Button>
+        <Button link size="lg" icon={MdOutlineFileDownload}>
+          Download
+        </Button>
+      </div>
+      <div className="p-3">
+        <NavLink active>Home</NavLink>
+        <NavLink>Services</NavLink>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/UI/Button/Button.jsx
@@ -0,0 +1,47 @@
+import cn from "classnames";
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+export const Button = ({ children, ...props }) => {
+  const { primary, secondary, link, icon: Icon, iconPosition = "left", size, ...attrs } = props;
+
+  const classes = cn(
+    { [styles.btnPrimary]: primary },
+    { [styles.btnSecondary]: secondary },
+    { [styles.btnLink]: link },
+    { [styles.btnLarge]: size === "lg" }
+  );
+
+  const iconClasses = cn("icon text-base", { "text-xl": size === "lg" });
+
+  const showLeftIcon = Icon && iconPosition === "left";
+  const showRightIcon = Icon && iconPosition === "right";
+
+  return (
+    <>
+      <button {...attrs} className={classes}>
+        <div className="flex justify-center items-center gap-2">
+          {showLeftIcon && (
+            <span className={iconClasses}>
+              <Icon />
+            </span>
+          )}
+          <div>{children}</div>
+          {showRightIcon && (
+            <span className={iconClasses}>
+              <Icon />
+            </span>
+          )}
+        </div>
+      </button>
+    </>
+  );
+};
+
+Button.propTypes = {
+  primary: PropTypes.bool,
+  secondary: PropTypes.bool,
+  icon: PropTypes.func,
+  iconPosition: PropTypes.oneOf(["left", "right"]),
+  size: PropTypes.oneOf(["lg", "sm"]),
+};
