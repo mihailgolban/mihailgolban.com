cd02ae633b5ff8fac87d2b7f0015d74466055bc3
Mihail Golban <mihail.golban@communityfibre.co.uk>
2023-03-06 10:20:27 +0200

    feat: add experience section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,27 +5,25 @@ import { Logo } from "../Logo";
 
 export const Header = () => {
   return (
-    <>
-      <header>
-        <div className="container mx-auto">
-          <div className="flex justify-between items-center h-[100px]">
-            <div className="header__logo">
-              <Logo />
-            </div>
-            <nav className="flex gap-12">
-              <NavLink active>Home</NavLink>
-              <NavLink>Services</NavLink>
-              <NavLink>Projects</NavLink>
-              <NavLink>Contacts</NavLink>
-            </nav>
-            <div className="header__buttons">
-              <Button primary icon={MdMailOutline} iconPosition="right">
-                Hire me
-              </Button>
-            </div>
+    <header className="bg-[#F9F9FC]">
+      <div className="container mx-auto ">
+        <div className="flex justify-between items-center h-[100px]">
+          <div className="header__logo">
+            <Logo />
+          </div>
+          <nav className="flex gap-12">
+            <NavLink active>Home</NavLink>
+            <NavLink>Services</NavLink>
+            <NavLink>Projects</NavLink>
+            <NavLink>Contacts</NavLink>
+          </nav>
+          <div className="header__buttons">
+            <Button primary icon={MdMailOutline} iconPosition="right">
+              Hire me
+            </Button>
           </div>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 };
