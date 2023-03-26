0d299e3a3125f6eee16dcee138cbc8d9663a3a87
Mihail Golban <mihail.golban@communityfibre.co.uk>
2023-03-26 15:33:18 +0300

    feat: add services section

--- src/components/sections/ExperienceSection.jsx
+++ src/components/sections/ExperienceSection.jsx
@@ -3,8 +3,8 @@ import { getExperience } from "@/lib/api";
 export const ExperienceSection = () => {
   const experience = getExperience();
   return (
-    <div id="experience-section" className="min-h-[300px] py-20">
-      <div className="flex max-w-7xl mx-auto gap-40 justify-center">
+    <div id="experience-section" className="min-h-[300px] flex flex-col justify-center items-center pt-24 pb-28">
+      <div className="grid grid-cols-3 mx-auto gap-40">
         {experience.map((item) => (
           <div key={item.title} className="flex flex-col items-center gap-4">
             <h2 className="text-5xl font-semibold text-primary">{item.count}+</h2>
--- src/lib/api.jsx
+++ src/lib/api.jsx
@@ -1,3 +1,7 @@
+import { ReactComponent as BackendIcon } from "@/assets/svc-backend-min.svg";
+import { ReactComponent as FrontendIcon } from "@/assets/svc-frontend-min.svg";
+import { ReactComponent as SupportIcon } from "@/assets/svc-support-min.svg";
+
 export function getExperience() {
   return [
     {
@@ -11,6 +15,29 @@ export function getExperience() {
     {
       title: "years of experience",
       count: 10,
+    },
+  ];
+}
+
+export function getServices() {
+  return [
+    {
+      title: "Web Development",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae blandit lectus. Praesent at hendrerit velit, at dictum nisl.",
+      icon: <FrontendIcon />,
+    },
+    {
+      title: "Mobile Development",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae blandit lectus. Praesent at hendrerit velit, at dictum nisl.",
+      icon: <BackendIcon />,
+    },
+    {
+      title: "UI/UX Design",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae blandit lectus. Praesent at hendrerit velit, at dictum nisl.",
+      icon: <SupportIcon />,
     },
   ];
 }
